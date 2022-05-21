75269ed5edb925bdbd81a688ff7cd2240f2d5965
Regilyn Feliciano <regilyn.feliciano@gmail.com>
2022-05-20 23:46:58 -0600

    O there were more

--- store/actions.tsx
+++ store/actions.tsx
@@ -1,25 +1,20 @@
-import Store from '.';
-
+import Store from ".";
 export const setMenuOpen = open => {
   Store.update(s => {
     s.menuOpen = open;
   });
 };
-
 export const setNotificationsOpen = open => {
   Store.update(s => {
     s.notificationsOpen = open;
   });
 };
-
 export const setSettings = settings => {
   Store.update(s => {
     s.settings = settings;
   });
 };
-
 // App-specific actions
-
 export const setDone = (list, item, done) => {
   Store.update((s, o) => {
     const listIndex = o.lists.findIndex(l => l === list);
