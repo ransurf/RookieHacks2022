9fce09b61986a84a437a51a5ec4213a683e712fe
Alx-Li <72467504+Alx-Li@users.noreply.github.com>
2022-05-22 00:16:33 -0700

    footer fix

--- components/TabSwitcher.tsx
+++ components/TabSwitcher.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
-
+import { IonButton } from '@ionic/react';
+import Router from 'next/router';
 interface Props {
   view?: string;
 }
@@ -9,12 +10,12 @@ const TabSwitcher = (props: Props) => {
   const { view } = props;
   return (
     <div className="text-white flex gap-8 justify-center mb-4">
-        <Link href={`/${view}/requests`}>
-            <a>Requests</a>
-        </Link>
-        <Link href={`/${view}/profile`}>
-            <a>Profile</a>
-        </Link>
+        <IonButton onClick={() => Router.push(`/${view}/requests`)}>
+            Requests
+        </IonButton>
+        <IonButton onClick={() => Router.push('/profile')}>
+          Profile
+        </IonButton>
     </div>
   )
 }
